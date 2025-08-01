ffba3c00df81a91b8bef0dfcf32fda3ae004a825
MMD_PROJECT <mmdvioleti@gmail.com>
2025-08-02 00:14:02 +0330

    COMPLETED - memory

--- memory.py
+++ memory.py
@@ -1,4 +1,24 @@
+from collections import deque
+import numpy as np
 
-# TODO
 class Memory:
-    pass
+    def __init__(self, max_num_transitions, mini_batch_size):
+        self.q = deque([], max_num_transitions)
+        self.max_num_transitions = max_num_transitions
+        self.mini_batch_size = mini_batch_size
+
+    # this function returns the number of stored transitions
+    def get_num_of_curr_transitions(self):
+        return len(self.q)
+    
+    def append(self, curr_state, action, reward, next_state, done):
+        trans = (curr_state, action, reward, next_state, done)
+        self.q.append(trans)
+    
+    def sample_mini_batch(self):
+        if len(self.q) < self.mini_batch_size:
+            raise Exception('Don\'t try to sample mini-batches while number of stored transitions < mini_batch_size')
+        
+        idxs = np.random.randint(0, len(self.q), self.mini_batch_size)
+        batch = (self.q[idx] for idx in idxs)
+        return batch
