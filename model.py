28d281ae7e4fb5531c9cd1c843396f896b597015
MMD_PROJECT <mmdvioleti@gmail.com>
2025-08-01 12:01:01 +0330

    DEFINE network architecture

--- model.py
+++ model.py
@@ -3,4 +3,49 @@ import torch.nn as nn
 
 # TODO
 class DQNNet(nn.Module):
-    pass
+    def __init__(self):
+        super(DQNNet, self).__init__()
+
+        # images should be preprocessed (extract luminance channel from RGB channels) by φ defined in the paper.
+
+        # in: (4, 84, 84) - out: (32, 20, 20)
+        # reLU should be applied on the outputs
+        self.conv1 = nn.Conv2d(inchannels=4, out_channels=32, kernel_size= (8, 8), stride=4) 
+
+        # in: (32, 20, 20) - out: (64, 9, 9)
+        # reLU should be applied on the outputs
+        self.conv2 = nn.Conv2d(inchannels=32, out_channels=64, kernel_size= (4, 4), stride=2)
+
+        # in: (64, 9, 9) - out: (64, 7, 7)
+        # reLU should be applied on the outputs
+        self.conv3 = nn.Conv2d(inchannels=64, out_channels=64, kernel_size= (3, 3), stride=1)
+
+        # flattening should be applied here feeding into fc1
+
+        # in: 64 * 7 * 7 = (3136, ) - out: (512, ) 
+        # reLU should be applied on the outputs
+        self.fc1 = nn.Linear(in_features=3136, out_features=512)
+
+        # in: (512, ) - out: (7, )
+        self.fc2 = nn.Linear(in_features=512, out_features=7)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.ReLU(x)
+         
+        x = self.conv2(x)
+        x = nn.ReLU(x)
+         
+        x = self.conv3(x)
+        x = nn.ReLU(x)
+
+        # TOOD: I GOTTA CHECK *start_dim* LATER.
+        x = nn.Flatten(start_dim=1, end_dim=-1)
+
+        x = self.fc1(x)
+        x = nn.ReLU(x)
+
+        x = self.fc2(x)
+        return nn.ReLU(x)
+         
+        
