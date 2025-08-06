2c3bd96bab5e728884e82b9f91a2d2fc37c5774d
MMD_PROJECT <mmdvioleti@gmail.com>
2025-08-06 18:28:23 +0330

    Fix EpisodicLifeEnv

--- wrappers.py
+++ wrappers.py
@@ -130,26 +130,59 @@ class FireResetEnv(gym.Wrapper):
                 successful_reset = True
         return obs, info
     
-class EpisodicLifeEnv(gym.Wrapper):
+class EpisodicLifeEnv(gym.Wrapper[np.ndarray, int, np.ndarray, int]):
+    # adapted from github.com/iewug/Atari-DQN
     """
-    Gym wrapper to announce game over even if the agent losses a single life
+    Make end-of-life == end-of-episode, but only reset on true game over.
+    Done by DeepMind for the DQN and co. since it helps value estimation.
 
     :param env: Environment to wrap
     """
-    def __init__(self, env):
+
+    def __init__(self, env: gym.Env) -> None:
         super().__init__(env)
+        self.lives = 0
+        self.was_real_done = True
+
+    def step(self, action: int):
+        obs, reward, terminated, truncated, info = self.env.step(action)
+        self.was_real_done = terminated or truncated
+
+        # check current lives, make loss of life terminal,
+        # then update lives to handle bonus lives
+        lives = self.env.unwrapped.ale.lives()  # type: ignore[attr-defined]
+        if 0 < lives < self.lives:
+            # for Qbert sometimes we stay in lives == 0 condition for a few frames
+            # so its important to keep lives > 0, so that we only reset once
+            # the environment advertises done.
+            terminated = True
+
+        self.lives = lives
+        return obs, reward, terminated, truncated, info
 
     def reset(self, *, seed = None, options = None):
-        obs, info = self.env.reset(seed=seed, options=options)
-        self.initial_lives = self.env.unwrapped.ale.lives()
+        """
+        Calls the Gym environment reset, only when lives are exhausted.
+        This way all states are still reachable even though lives are episodic,
+        and the learner need not know about any of this behind-the-scenes.
+
+        :param kwargs: Extra keywords passed to env.reset() call
+        :return: the first observation of the environment
+        """
+        if self.was_real_done:
+            obs, info = self.env.reset(seed=seed, options=options)
+        else:
+            # no-op step to advance from terminal/lost life state
+            obs, _, terminated, truncated, info = self.env.step(0)
+
+            # The no-op step can lead to a game over, so we need to check it again
+            # to see if we should reset the environment and avoid the
+            # monitor.py `RuntimeError: Tried to step environment that needs reset`
+            if terminated or truncated:
+                obs, info = self.env.reset(seed=seed, options=options)
+
+        self.lives = self.env.unwrapped.ale.lives()  # type: ignore[attr-defined]
         return obs, info
-    
-    def step(self, action):
-        obs, reward, terminated, truncated, info = self.env.step(action)
-        lives = self.env.unwrapped.ale.lives()
-        if lives < self.initial_lives:
-            terminated = True
-        return obs, reward, terminated, truncated, info
     
 class BreakoutActionTransform(gym.Wrapper):
     """
