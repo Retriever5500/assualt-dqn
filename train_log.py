8cb34ff229821f1d94470b9fd56645b7dfd62701
Retriever5500 <Kilowatt7028@tutanota.com>
2025-08-11 16:22:46 +0330

    Plot average rewards and losses

--- train_log.py
+++ train_log.py
@@ -5,19 +5,34 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+def rolling_average(data, window_size):
+    """Calculate the rolling average of a given data list."""
+    averages = []
+    for i in range(len(data)):
+        # Calculate the average from the start to the current index if i < window_size
+        if i < window_size:
+            avg = np.mean(data[:i + 1])  # Average from the start to the current index
+        else:
+            avg = np.mean(data[i - window_size + 1:i + 1])  # Average of the last 'window_size' elements
+        averages.append(avg)
+    return averages
+
 
 def plot_logs(game_id, total_interactions, episode_cnt, history_of_total_losses, history_of_total_rewards, plot_dir_path):
     fig, axs = plt.subplots(1, 2, figsize=(10, 5))
     axs = axs.flatten()
     x = np.arange(1, episode_cnt + 1)
 
-    sns.lineplot(x=x, y=history_of_total_losses, ax=axs[0])
+    rolling_avg_losses = rolling_average(history_of_total_losses, 200)
+    rolling_avg_rewards = rolling_average(history_of_total_rewards, 200)
+
+    sns.lineplot(x=x, y=rolling_avg_losses, ax=axs[0])
     axs[0].set_title('Total Training Loss over each Episode')
     axs[0].set_xlabel('Episodes')
     axs[0].set_ylabel('Total MSE Loss')
     # axs[0].legend()
 
-    sns.lineplot(x=x, y=history_of_total_rewards, ax=axs[1])
+    sns.lineplot(x=x, y=rolling_avg_rewards, ax=axs[1])
     axs[1].set_title('Total Training Rewards over each Episode')
     axs[1].set_xlabel('Episodes')
     axs[1].set_ylabel('Total Reward')
